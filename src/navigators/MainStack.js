903a22ae81f2129d3c07137ad5345f52b670d2ca
andre.silva <andre.silva@marimex.com.br>
2024-06-17 08:20:42 -0300

    TabBars - Projeto Final

--- src/navigators/MainStack.js
+++ src/navigators/MainStack.js
@@ -3,12 +3,24 @@ import { createStackNavigator } from "@react-navigation/stack";
 
 import HomeScreen from "../pages/HomeScreen";
 import AboutScreen from "../pages/AboutScreen";
+import { Button, Image, Text } from "react-native";
+
+import Logo from "../components/Logo";
 
 const MainStack = createStackNavigator();
 
 export default () => {
     return (
-        <MainStack.Navigator>
+        <MainStack.Navigator screenOptions={{
+            headerTitleAlign: "center",
+            headerStyle: {
+                backgroundColor: "#66FF66",
+            },
+            headerTitleStyle: {
+               color: "#000",
+               fontSize: 16 
+            }
+        }}>
             <MainStack.Screen name="Home"  component={HomeScreen} />
             <MainStack.Screen name="About"  component={AboutScreen} />
         </MainStack.Navigator>
--- src/pages/HomeScreen.js
+++ src/pages/HomeScreen.js
@@ -1,4 +1,4 @@
-import React, {useState } from "react";
+import React, { useState, useLayoutEffect } from "react";
 import { View, Text, StyleSheet, Button, TextInput } from "react-native";
 import { useNavigation } from "@react-navigation/native";
 
@@ -7,25 +7,79 @@ function HomeScreen() {
     const navigation = useNavigation();
 
     const [name, setName] = useState("");
+    const [count, setCount] = useState(0);
 
-    const handleSendButton = () => {
-       navigation.navigate('About', {name: name});
+    useLayoutEffect(() => {
+        navigation.setOptions({
+            title: count,
+        });
+    }, [count]);
 
+    useLayoutEffect(() => {
+        navigation.setOptions({
+            headerRight: () => <Button  title="+1" onPress={handleHeaderPlus}/>
+        });
+    }, []);
+
+    const handleHeaderPlus = () => {
+        setCount((c) => {
+            return c + 1;
+        })  ;
     }
 
+    const handleSendButton = () => {
+
+    navigation.setOptions({
+        title: name
+    });
+    //    navigation.navigate('About', {
+    //     //name: name || undefined ,
+    //     cor: name || undefined 
+    };
+
+    
+
     return (
+
         <View style={styles.container}>
             <Text>Qual seu nome ?</Text>
             <TextInput 
             style={styles.input} 
             value={name} 
             onChangeText={t => setName(t)} 
+            keyboardType="default"
             />
 
             <Button  
             title="Enviar" 
             onPress={handleSendButton} 
             />
+
+            <Button 
+            title="BG Azul"
+            onPress={() => navigation.setOptions({
+                headerStyle: {
+                    backgroundColor: "#0000FF"
+                }
+                })}
+            />
+
+            <Button 
+            title="BG Vermelho"
+            onPress={() => navigation.setOptions({
+                headerStyle: {
+                    backgroundColor: "#FF0000"
+                }
+                })}
+            />
+
+            <Button 
+            title="+1" 
+            onPress={() => setCount(count + 1)}    
+            />
+
+           
+
         </View>
     );
 }
